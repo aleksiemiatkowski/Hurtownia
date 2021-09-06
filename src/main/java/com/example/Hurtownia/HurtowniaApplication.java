cbe4df2cab54f04aba23367fe1871e62b828da2e
Aleksander Siemiątkowski <aleksiemiatkowski@gmail.com>
2021-09-06 17:57:11 +0200

    DODANIE PLIKÓW STARTOWYCH

---
+++ src/main/java/com/example/Hurtownia/HurtowniaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hurtownia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurtowniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurtowniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hurtownia/HurtowniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hurtownia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HurtowniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
